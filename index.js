bee32b888ee4f2393fe99c62d491ffbfcc2e3651
AkashiSN <btorntireinvynriy@gmail.com>
2018-04-22 20:09:09 +0900

    BUG Fix

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-
 function renderLink(content,user_name,repo_name,file_path){
-    let result = "";
-    while(~content.indexOf('[[')){
-        let idx1 = content.indexOf('[[');
-        let idx2 = content.indexOf(']]');
-        let file_name = content.slice(idx1+2,idx2);
-        let file = file_path + "/" + file_name;
-        let link = `<a href="https://github.com/${user_name}/${repo_name}/blob/master/${file}" target="_blank">${file_name}</a>`;
-        result += content.slice(0,idx1)+link;
-        content = content.substr(idx2+2);
-        if(content.indexOf('[[')){
-           result+= content;
-           break;
+    if(~content.indexOf('[[')){
+        let result = "";
+        while (~content.indexOf('[[')) {
+            let idx1 = content.indexOf('[[');
+            let idx2 = content.indexOf(']]');
+            let file_name = content.slice(idx1 + 2, idx2);
+            let file = file_path + "/" + file_name;
+            let link = `<a href="https://github.com/${user_name}/${repo_name}/blob/master/${file}" target="_blank">${file_name}</a>`;
+            result += content.slice(0, idx1) + link;
+            content = content.substr(idx2 + 2);
+            if (content.indexOf('[[')) {
+                result += content;
+                break;
+            }
         }
+        return result;
     }
-    return result;
+    return content;
 }
 
 module.exports = {
